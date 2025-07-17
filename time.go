9958aa1971113274d3adf53526cf97a20ad94fa0
markeckles <2435965705@qq.com>
2025-07-17 10:50:21 +0800

    提交

---
+++ time.go
@@ -0,0 +1,39 @@
+package tool
+
+import "time"
+
+// RangeOfMonth 某时间所在月的开始时间和结束时间
+func RangeOfMonth(day time.Time) (first, last time.Time) {
+	first = time.Date(day.Year(), day.Month(), 1, 0, 0, 0, 0, day.Location())
+	nextMonth := first.AddDate(0, 1, 0)
+	last = time.Date(
+		nextMonth.Year(),
+		nextMonth.Month(),
+		0,
+		23,
+		59,
+		59,
+		int(time.Second-time.Nanosecond),
+		day.Location(),
+	)
+	return
+}
+
+// DaysInMonth 某时间所在月的天数
+func DaysInMonth(day time.Time) int {
+	lastOfThisMonth := time.Date(day.Year(), day.Month()+1, 0, 0, 0, 0, 0, day.Location())
+	return lastOfThisMonth.Day()
+}
+
+// WeeksInMonth 某时间所在月的周的数量
+func WeeksInMonth(day time.Time) int {
+	year, month, _ := day.Date()
+	firstOfMonth := time.Date(year, month, 1, 0, 0, 0, 0, day.Location())
+	count := 0
+	for d := firstOfMonth; d.Month() == month; d = d.AddDate(0, 0, 1) {
+		if d.Weekday() == time.Monday {
+			count++
+		}
+	}
+	return count
+}
